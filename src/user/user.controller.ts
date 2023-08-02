346a0caaede16f8abda4ee28f39c4ab145ff0d8f
fernandes <eduardo.yugan@gmail.com>
2023-08-02 00:50:38 -0300

    feat: get user profile by token

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,6 +2,7 @@ import {
   Controller,
   Post,
   Get,
+  Req,
   Body,
   UseGuards,
   HttpException,
@@ -12,6 +13,12 @@ import { UserDto } from './dto/user.dto/user.dto';
 import { User } from './entities/user/user';
 import { HashPassword } from '../auth/hash-password/hash-password';
 import { AuthGuard } from '../auth/auth.guard';
+import { Request } from 'express';
+
+interface IUserProfile {
+  name: string;
+  email: string;
+}
 
 @Controller('/user')
 export class UserController {
@@ -45,5 +52,21 @@ export class UserController {
   @Get('/')
   getAll(): Promise<User[]> {
     return this.userService.findAll();
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('/profile')
+  async getProfile(@Req() request: Request): Promise<IUserProfile> {
+    const { email } = request.user as User;
+
+    const user = await this.userService.findByEmail(email);
+
+    if (!user)
+      throw new HttpException(
+        { message: 'User not found', status: HttpStatus.NOT_FOUND },
+        HttpStatus.NOT_FOUND,
+      );
+
+    return { name: user.name, email: user.email };
   }
 }
